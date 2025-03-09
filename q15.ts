e5764211abd68ee0da717a790f786fc2521c671d
Udayasaran-kumar <ssaran3091998@gmail.com>
2025-03-09 20:13:55 +0530

    q15 added

---
+++ q15.ts
@@ -0,0 +1,28 @@
+class Vehicle {
+    make: string;
+    model: string;
+    year: number;
+  
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+    getDetails(): string {
+        return `Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`;
+      }
+    }
+     class Car extends Vehicle {
+        fuelType: string;
+      
+        constructor(make: string, model: string, year: number, fuelType: string) {
+          super(make, model, year); // Call the constructor of the base class
+          this.fuelType = fuelType;
+        }
+        getDetails(): string {
+            return `${super.getDetails()}, Fuel Type: ${this.fuelType}`;
+          }
+}
+const myCar = new Car("Toyota", "Corolla", 2021, "Petrol");
+console.log(myCar.getDetails()); 
+// Output: Make: Toyota, Model: Corolla, Year: 2021, Fuel Type: Petrol
